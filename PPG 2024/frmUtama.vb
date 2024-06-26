3dd3d2fe90e9cc3c3c1805725fba4a942ba1c6d8
thre360 <agrvbelpp@gmail.com>
2024-06-26 18:14:31 +0700

    fix frmUtama

--- PPG 2024/frmUtama.vb
+++ PPG 2024/frmUtama.vb
@@ -42,8 +42,4 @@
     Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
 
     End Sub
-
-    Private Sub frmUtama_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
 End Class
